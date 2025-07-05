0452562e197e22bece7584c832a06dff46cd6ebe
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-07-05 21:13:44 +0530

    Create foreach_loop(simple).sv

---
+++ LOOPS/foreach_loop/foreach_loop(simple).sv
@@ -0,0 +1,23 @@
+module foreach_loop;
+  int arr[7:0];
+  
+  initial begin
+    arr = '{0,1,2,3,4,5,6,7};
+    foreach(arr[i])begin
+      $display("arr[%0d] = %0d",arr[i],i);
+    end
+  end
+endmodule
+
+
+
+# KERNEL: arr[0] = 7
+# KERNEL: arr[1] = 6
+# KERNEL: arr[2] = 5
+# KERNEL: arr[3] = 4
+# KERNEL: arr[4] = 3
+# KERNEL: arr[5] = 2
+# KERNEL: arr[6] = 1
+# KERNEL: arr[7] = 0
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
