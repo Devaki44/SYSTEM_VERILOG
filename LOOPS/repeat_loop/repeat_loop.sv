5f146b4ba9e9728ca1da42131cb963f593cc5646
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-07-05 20:39:28 +0530

    Create repeat_loop.sv

---
+++ LOOPS/repeat_loop/repeat_loop.sv
@@ -0,0 +1,18 @@
+module repeat_loop;
+  
+  initial begin
+    repeat(5)begin
+      $display("BELIEVE YOURSELF");
+    end
+  end
+endmodule
+
+
+
+# KERNEL: BELIEVE YOURSELF
+# KERNEL: BELIEVE YOURSELF
+# KERNEL: BELIEVE YOURSELF
+# KERNEL: BELIEVE YOURSELF
+# KERNEL: BELIEVE YOURSELF
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
