09be612e832bc4c4b8cdd5697b31e8ca7b97f3d1
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-07-06 13:12:37 +0530

    Create unique_if_with_else(one_condition_true).sv.sv

---
+++ Control_flow/Conditional_statements/unique_if_with_else(one_condition_true).sv.sv
@@ -0,0 +1,23 @@
+module if_statement;
+  int a = 50;
+  int b = 20;
+  
+  initial begin
+    unique if(a<b)begin
+      $display("A is less than B");
+    end 
+    else if(b<a) begin
+      $display("B is less than A");
+    end 
+    else begin
+      $display("A is equal to B");
+    end
+  end
+  
+endmodule
+
+
+
+# KERNEL: B is less than A
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
