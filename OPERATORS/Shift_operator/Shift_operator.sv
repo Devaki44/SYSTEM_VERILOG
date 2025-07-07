dcc9f29772fc9c3c34545f95bd5b8021aaca481e
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-07-07 18:35:21 +0530

    Create Shift_operator.sv

---
+++ OPERATORS/Shift_operator/Shift_operator.sv
@@ -0,0 +1,20 @@
+module shift_operator;
+  logic unsigned[7:0]a;
+  
+  initial begin
+    a = 8'b1100_1011;
+    
+    $display("Logical_Right_Shift:%b",a>>1);
+    $display("Logical_Left_Shift:%b",a<<1);
+    $display("Arithmetic_Right_Shift=%b",a>>>2);
+    $display("Arithmetic_Left_Shift=%b",a<<<1);
+  end
+endmodule
+
+
+# KERNEL: Logical_Right_Shift:01100101
+# KERNEL: Logical_Left_Shift:10010110
+# KERNEL: Arithmetic_Right_Shift=00110010
+# KERNEL: Arithmetic_Left_Shift=10010110
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
