23c58658aab4fc0cb3ff8ff70fe9b5389d12cffd
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-07-07 17:01:07 +0530

    Create Concatenation_operator.sv

---
+++ OPERATORS/Concatenation_operator.sv
@@ -0,0 +1,21 @@
+module concatenation_operator;
+  logic [7:0]out;
+  logic [3:0]data1,data2;
+  string out_string;
+  
+  initial begin
+    data1 = 4'b1101;
+    data2 = 4'bx101;
+    out = {data1 , data2};
+    $display("out : %b",out);
+    
+    out_string = {"Hi"," ","Devaki"};
+    $display("out_string : %p",out_string);
+  end
+endmodule
+
+
+# KERNEL: out : 1101x101
+# KERNEL: out_string : "Hi Devaki"
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
