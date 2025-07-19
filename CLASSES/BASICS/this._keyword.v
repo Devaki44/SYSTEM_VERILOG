e10a272d1e630ebbdfc9d4b60c85cc7f45f00f6a
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-07-19 22:56:49 +0530

    Update this._keyword.v

--- CLASSES/BASICS/this._keyword.v
+++ CLASSES/BASICS/this._keyword.v
@@ -2,12 +2,12 @@ class home;
   bit light=0;
   string sw="OFF";
   
-  task task1;
-    light = 1;
-    sw	  = "ON" ;
+  task task1();  
+	bit light=1 ;
+	string sw="ON";
     
-    this.light = light;
-    this.sw	   = sw	  ;	
+  	this.light = light;
+  	this.sw = sw	  ;	
     
     $display("INSIDE CLASS: The sw is %s ,so the light %0d",sw,light);
   endtask
@@ -21,16 +21,16 @@ module test;
     
     a.task1();
     
-    a.light = 0;
-    a.sw	  = "OFF" ;
-    $display("OUTSIDE CLASS: The sw is %s ,so the light %0d",a.sw,a.light);  
+    a.light = 0 ;
+  	a.sw = "off"	  ;	
     
-  end 
+    $display("OUTSIDE CLASS: The sw is %s ,so the light %0d",a.sw,a.light);
+  end
 endmodule
 
 
-
+//OUTPUT
 # KERNEL: INSIDE CLASS: The sw is ON ,so the light 1
-# KERNEL: OUTSIDE CLASS: The sw is OFF ,so the light 0
+# KERNEL: OUTSIDE CLASS: The sw is off ,so the light 0
 # KERNEL: Simulation has finished. There are no more test vectors to simulate.
 # VSIM: Simulation has finished.
