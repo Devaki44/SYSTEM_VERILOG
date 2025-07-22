2071ccd2173d01d842d3a5ea660332f5b91f913a
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-07-22 19:13:05 +0530

    Update int_to_integer.sv

--- CLASSES/BASICS/Casting/static_casting/int_to_integer.sv
+++ CLASSES/BASICS/Casting/static_casting/int_to_integer.sv
@@ -1,21 +1,22 @@
 module casting;
-  int a;
-  integer b;
+  integer a;
+  int b;
   
   initial begin
     a= 8'b00x1_xxxx;
     
-    $display(" %0b ",a);
+    $display(" %b ",a);
     
     b = int'(a);
-    $display(" %0b ",a);
+    $display(" %b ",b);
   end
 endmodule
+    
 
 
 
 //OUTPUT
-# KERNEL:  10000 
-# KERNEL:  10000 
+# KERNEL:  00000000000000000000000000x1xxxx 
+# KERNEL:  00000000000000000000000000010000 
 # KERNEL: Simulation has finished. There are no more test vectors to simulate.
 # VSIM: Simulation has finished.
