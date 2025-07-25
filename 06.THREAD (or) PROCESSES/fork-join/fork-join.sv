395c637ae85ab004f5bdc02aa100dcefa3c1904d
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-07-25 12:51:24 +0530

    Update fork-join.sv

--- 06.THREAD (or) PROCESSES/fork-join/fork-join.sv
+++ 06.THREAD (or) PROCESSES/fork-join/fork-join.sv
@@ -1,23 +1,25 @@
-module fork_join;
+module threads;
   
   initial begin
     fork
-      $display("----------------------");
+      $display("----------------------");  //this is consider as one thread or processes
       
       begin
-        $display("$time,process1 started");
+        $display("time=%0t,process1 started",$time);
         #5;
-        $display("$time,process1 finished");
+        $display("time=%0t,process1 finished",$time);
       end
       
       begin
-        $display("$time,process2 started");
+        $display("time=%0t,process2 started",$time);
         #20;
-        $display("$time,process2 finished");
+        $display("time=%0t,process2 finished",$time);
       end
     join
+    
+
     $display("----------------------");
-    $display("$time,outside block");
+    $display("time=%0t,outside block",$time);
     $display("----------------------");
   end
 endmodule
@@ -26,12 +28,12 @@ endmodule
 
 //OUTPUT
 # KERNEL: ----------------------
-# KERNEL: $time,process1 started
-# KERNEL: $time,process2 started
-# KERNEL: $time,process1 finished
-# KERNEL: $time,process2 finished
+# KERNEL: time=0,process1 started
+# KERNEL: time=0,process2 started
+# KERNEL: time=5,process1 finished
+# KERNEL: time=20,process2 finished
 # KERNEL: ----------------------
-# KERNEL: $time,outside block
+# KERNEL: time=20,outside block
 # KERNEL: ----------------------
 # KERNEL: Simulation has finished. There are no more test vectors to simulate.
 # VSIM: Simulation has finished.
