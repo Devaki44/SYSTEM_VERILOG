60fc393da78e09cb6997299432c463c681ff3d71
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-07-25 12:10:50 +0530

    Create fork-join_any.sv

---
+++ 06.THREAD (or) PROCESSES/fork-join_any/fork-join_any.sv
@@ -0,0 +1,36 @@
+module fork_join_any;
+  
+  initial begin
+    fork
+      $display("----------------------");  //this is consider as one thread or processes
+      
+      begin
+        $display("$time,process1 started");
+        #5;
+        $display("$time,process1 finished");
+      end
+      
+      begin
+        $display("$time,process2 started");
+        #20;
+        $display("$time,process2 finished");
+      end
+    join_any
+    $display("----------------------");
+    $display("outside block");
+    $display("----------------------");
+  end
+endmodule
+
+
+//OUTPUT
+# KERNEL: ----------------------
+# KERNEL: ----------------------
+# KERNEL: outside block
+# KERNEL: ----------------------
+# KERNEL: $time,process1 started
+# KERNEL: $time,process2 started
+# KERNEL: $time,process1 finished
+# KERNEL: $time,process2 finished
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
