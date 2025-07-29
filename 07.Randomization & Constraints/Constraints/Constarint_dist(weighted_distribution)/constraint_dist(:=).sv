01476fd689a3d0c18290ec3b4bfc9690898457c0
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-07-29 10:19:10 +0530

    Create constraint_dist(:=).sv

---
+++ 07.Randomization & Constraints/Constraints/Constarint_dist(weighted_distribution)/constraint_dist(:=).sv
@@ -0,0 +1,51 @@
+//CODE
+class packet;
+  rand bit[3:0] data ;
+  
+  constraint data_c{ data dist{2:=5 , [10:12]:=8}; }
+endclass
+
+module test;
+  packet pkt ;
+  initial begin
+    pkt = new();
+    repeat(29)begin
+     pkt.randomize();
+      $display(" data = %0d ",pkt.data);
+    end
+  end
+endmodule
+
+
+//OUTPUT
+# KERNEL:  data = 2 
+# KERNEL:  data = 12 
+# KERNEL:  data = 11 
+# KERNEL:  data = 2 
+# KERNEL:  data = 2 
+# KERNEL:  data = 11 
+# KERNEL:  data = 11 
+# KERNEL:  data = 12 
+# KERNEL:  data = 11 
+# KERNEL:  data = 10 
+# KERNEL:  data = 10 
+# KERNEL:  data = 12 
+# KERNEL:  data = 11 
+# KERNEL:  data = 2 
+# KERNEL:  data = 2 
+# KERNEL:  data = 11 
+# KERNEL:  data = 10 
+# KERNEL:  data = 2 
+# KERNEL:  data = 12 
+# KERNEL:  data = 2 
+# KERNEL:  data = 11 
+# KERNEL:  data = 10 
+# KERNEL:  data = 10 
+# KERNEL:  data = 11 
+# KERNEL:  data = 10 
+# KERNEL:  data = 12 
+# KERNEL:  data = 10 
+# KERNEL:  data = 11 
+# KERNEL:  data = 11 
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
