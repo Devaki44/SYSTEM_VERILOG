3bef740dd135158a54d10967a68f9b89dd3fa160
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-08-04 23:10:33 +0530

    Update Semaphores with many keys.sv

--- 06.THREAD (or) PROCESSES/SEMAPHORES/Semaphores with many keys.sv
+++ 06.THREAD (or) PROCESSES/SEMAPHORES/Semaphores with many keys.sv
@@ -3,15 +3,15 @@ module semaphore();
   semaphore sem = new(3);
   
   task Process_A();
-    sem.get(3);
+    sem.get(4);
     $display(" 1. Process_A started ");
     #5;
     $display(" 2. Process_A completed ");
-    sem.put(2);
+    sem.put(4);
   endtask
   
   task Process_B();
-    sem.get(2);
+    sem.get(5);
     $display(" 3. Process_B started");
     #4;
     $display(" 4. Process_B completed ");
@@ -29,8 +29,4 @@ endmodule
 
 //OUTPUT
 # run -all
-#  1. Process_A started 
-#  2. Process_A completed 
-#  3. Process_B started
-#  4. Process_B completed 
 # exit
