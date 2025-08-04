5fe7d884943abf30b93a4f8375ca516b5ffa36e4
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-08-04 23:07:44 +0530

    Create Put more keys Process_B.sv

---
+++ 06.THREAD (or) PROCESSES/SEMAPHORES/Put more keys Process_B.sv
@@ -0,0 +1,38 @@
+//CODE
+
+module semaphore();
+  semaphore sem = new(3);
+  
+  task Process_A();
+    sem.get(3);
+    $display(" 1. Process_A started ");
+    #5;
+    $display(" 2. Process_A completed ");
+    sem.put(5);
+  endtask
+  
+  task Process_B();
+    sem.get(5);
+    $display(" 3. Process_B started");
+    #4;
+    $display(" 4. Process_B completed ");
+    sem.put(6);
+  endtask
+  
+  initial begin
+   fork
+    Process_A() ;
+    Process_B() ;
+   join
+  end
+endmodule
+
+
+
+//OUTPUT
+# run -all
+#  1. Process_A started 
+#  2. Process_A completed 
+#  3. Process_B started
+#  4. Process_B completed 
+# exit
