51ee1c5b14816822565db6d735eb7f7ca6bcb4d5
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-08-29 16:12:36 +0530

    Create practice1.sv

---
+++ CLASSES/BASICS/07.super._keyword/practice1.sv
@@ -0,0 +1,42 @@
+class parent;
+  bit [7:0] data;
+  
+  function new();
+    data =8'hcd ;
+  endfunction
+  
+  function void display();
+    $display(" PARENT : data = %h ",data);
+  endfunction
+  
+endclass
+
+class child extends parent;
+  bit [7:0] data = 8'hff;
+  
+  function new();
+//     super.new();
+//     super.data = 8'hcd ;
+    super.display();
+  endfunction
+  
+  function void display();
+    $display(" CHILD : data = %h ",data);
+  endfunction
+  
+endclass
+
+module test;
+  child c1;
+  initial begin
+    c1 = new();
+    c1.display();
+    
+  end
+endmodule
+
+//OUTPUT
+# run -all
+#  PARENT : data = cd 
+#  CHILD : data = ff 
+# exit
