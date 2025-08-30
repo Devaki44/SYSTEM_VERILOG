4a39988ebc5afba07ee75ea3b3773400105e3d7b
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-08-30 16:40:22 +0530

    Create Encapsulation(protected).sv

---
+++ CLASSES/BASICS/09.Data_hiding&Encapsulation/PRACTICE/Encapsulation(protected).sv
@@ -0,0 +1,49 @@
+class parent ;
+  protected int a;
+  
+  function new();
+    a++;
+    $display(" PARENT : a = %0d ",a);
+  endfunction
+endclass
+
+class child extends parent;
+  
+  function new();
+    a++;
+    $display(" CHILD : a =%0d ",a);
+  endfunction
+endclass
+
+class child1 extends child;
+  
+  function new();
+    a++;
+    $display(" CHILD1 : a =%0d ",a);
+  endfunction
+endclass
+
+module test;
+  parent p;
+  child c;
+  child1 c1;
+  initial begin
+    p = new();
+    c = new();
+    c1 = new();
+    
+  end
+endmodule
+
+
+
+//OUTPUT
+xcelium> run
+ PARENT : a = 1 
+ PARENT : a = 1 
+ CHILD : a =2 
+ PARENT : a = 1 
+ CHILD : a =2 
+ CHILD1 : a =3 
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
