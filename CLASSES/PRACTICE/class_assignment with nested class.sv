96c18623846d0ee3ead6afe36ca36b1134d3b1a9
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-09-17 23:23:37 +0530

    Create class_assignment with nested class.sv

---
+++ CLASSES/PRACTICE/class_assignment with nested class.sv
@@ -0,0 +1,57 @@
+
+class home;
+  int data; 
+ // 
+  class room;
+  int addr;
+  
+  function new();
+    addr = 8'hee;
+  endfunction
+   
+  function void display();
+    $display(" addr = %0h ",addr);
+  endfunction
+endclass
+   //
+  room rm;
+ 
+  function new();
+    data = 8'hff ;
+    rm = new();
+  endfunction
+  function void display();
+    $display(" data = %0h ",data);
+  endfunction
+endclass
+
+
+module test;
+  home h1,h2;
+  initial begin
+    h1 = new();
+    h2 = h1;
+    h1.display();
+    h2.display();
+    h1.rm.display();
+    h2.rm.display();
+    h1.data = 8'hde;
+    h1.rm.addr = 8'h04;
+    h1.display();
+    h2.display();
+  end
+endmodule
+
+
+
+
+
+//output
+# run -all
+#  data = ff 
+#  data = ff 
+#  addr = ee 
+#  addr = ee 
+#  data = de 
+#  data = de 
+# exit
